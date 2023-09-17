aa816b2ff939736a7049edb45b1824ab6cebda60
Mayank Kumar <96490105+mank-423@users.noreply.github.com>
2023-09-17 19:16:58 +0530

    Working v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Hero from './components/Hero'
+import Demo from './components/Demo'
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      APP
-    </div>
+    <main>
+        <div className="main">
+            <div className="gradient">
+
+            </div>
+        </div>
+
+        <div className="app">
+            <Hero />
+            <Demo />
+        </div>
+    </main>
   )
 }
 
---
+++ src/services/article.js
@@ -0,0 +1,39 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
+export const articleApi = createApi({
+    reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', rapidApiKey);
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getSummary: builder.query({
+            // encodeURIComponent() function encodes special characters that may be present in the parameter values
+            // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
+            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+        }),
+    }),
+})
+
+export const { useLazyGetSummaryQuery } = articleApi
+
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+//     params: {
+//         url: 'https://time.com/6266679/musk-ai-open-letter/',
+//         length: '3'
+//     },
+//     headers: {
+//         'X-RapidAPI-Key': '66484d3c37msha3c8502fa25ab07p100f45jsn3cd64d418129',
+//         'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
+//     }
+// };
